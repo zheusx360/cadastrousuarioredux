6e78713da014f643564e897f8fbc33821a73dc00
Rodrigo <rodrigo@kyte.com.br>
2023-01-12 12:14:20 -0300

    ajustes prettier eslint

--- App.tsx
+++ App.tsx
@@ -2,23 +2,22 @@ import React from 'react';
 import Screens from './src/screens';
 import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
-import store from './src/store'
+import store from './src/store';
 
-StatusBar.setBarStyle('dark-content')
-StatusBar.setBackgroundColor('transparent')
-StatusBar.setTranslucent(true)
+StatusBar.setBarStyle('dark-content');
+StatusBar.setBackgroundColor('transparent');
+StatusBar.setTranslucent(true);
 
 if (__DEV__) {
-  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'))
+  import('./ReactotronConfig').then(() => console.log('Reactotron Configured'));
 }
-
 
 const App = () => {
   return (
     <Provider store={store}>
       <Screens />
     </Provider>
-  )
+  );
 };
 
 export default App;
